af6febc40b1f76ba48dfe59f274016ed78567883
Abhishek Ranjan <ranjanabhi3158@gmail.com>
2026-09-17 20:02:23 +0530

    routes established

--- aegis-gateway/app/api/router.py
+++ aegis-gateway/app/api/router.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 from app.modules.auth.routes import auth_router
 from app.modules.user.routes import user_router
 from app.modules.token.routes import token_router
+from app.modules.key.routes import key_router
 
 api_router=APIRouter(prefix="/api/v1")
 
 api_router.include_router(auth_router)
 api_router.include_router(user_router)
 api_router.include_router(token_router)
+api_router.include_router(key_router)
---
+++ aegis-gateway/app/modules/key/routes.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter,Depends,status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import User
+from app.api.deps import get_current_user
+from app.db.session import get_db
+from app.modules.key.service import KeyService
+from app.modules.key.schemas import (
+  GenerateResponse,
+  GenerateSchema,
+  KeySchema
+)
+
+key_router=APIRouter(prefix="/api-keys",tags=["API Keys"])
+
+@key_router.post("/generate",response_model=GenerateResponse,status_code=status.HTTP_201_CREATED)
+async def generate(data:GenerateSchema,user:User=Depends(get_current_user),db:AsyncSession=Depends(get_db)):
+  """key generation request"""
+  return await KeyService.generate_key(data,user.id,db)
+
+@key_router.get("/all",response_model=list[KeySchema],status_code=status.HTTP_200_OK)
+async def get_all_keys(user:User=Depends(get_current_user),db:AsyncSession=Depends(get_db)):
+  """request for all the keys of user"""
+  return await KeyService.get_all_keys(user.id,db)
+
+@key_router.get("/{key_id}",response_model=KeySchema,status_code=status.HTTP_200_OK)
+async def get_key(key_id:int,user:User=Depends(get_current_user),db:AsyncSession=Depends(get_db)):
+  return await KeyService.get_key(key_id,user.id,db)
+
+@key_router.post("{key_id}/deactivate",response_model=None,status_code=status.HTTP_204_NO_CONTENT)
+async def deactivate(key_id:int,user:User=Depends(get_current_user),db:AsyncSession=Depends(get_db)):
+  await KeyService.deactivate_key(key_id,user.id,db)
