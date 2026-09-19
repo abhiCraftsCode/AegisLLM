e6956d2690ff05e504eb2ab847f7735208e2fe6f
Abhishek Ranjan <ranjanabhi3158@gmail.com>
2026-09-19 18:59:36 +0530

    profile-update added

--- aegis-gateway/app/modules/user/schemas.py
+++ aegis-gateway/app/modules/user/schemas.py
@@ -36,3 +36,32 @@ class ProfileSchema(UserSchema):
   oauth_provider:str|None=None
   is_active:bool
   created_at:datetime
+
+class UpdateSchema(BaseModel):
+  """request schema for profile updation."""
+  name:str|None=Field(default=None,min_length=2,max_length=100)
+  email:EmailStr|None=None
+  phone:str|None=Field(default=None,max_length=15,min_length=10)
+  password:str|None=Field(default=None,max_length=128,min_length=8)
+  @field_validator("phone",mode="before")
+  @classmethod
+  def validate_phone(cls, value):
+    if value is None:
+        return None
+    if not isinstance(value, str):
+        raise ValueError("Phone number must be a string.")
+    value = value.strip()
+    if value.startswith("+91"):
+        number = value[3:]
+    else:
+        number = value
+    if len(number) != 10:
+        raise ValueError("Indian phone number must contain 10 digits.")
+    if not number.isdigit():
+        raise ValueError("Phone number must contain only digits.")
+    if number[0] not in "6789":
+        raise ValueError(
+            "Indian phone number must start with 6, 7, 8, or 9."
+        )
+    return f"+91{number}"
+  
